fe8ff920be8db671762994879aed5727a15674d4
lizat-i <ivan.lizat@outlook.de>
2021-12-14 15:43:11 +0100

    adjusted startfile

--- MatlabStart.sh
+++ MatlabStart.sh
@@ -4,5 +4,6 @@
 export MATLABPATH=/opt/ibm/ILOG/CPLEX_Enterprise_Server1210/CPLEX_Studio/cplex/matlab/x86-64_linux:/opt/ibm/ILOG/CPLEX_Enterprise_Server1210/CPLEX_Studio/cplex/examples/src/matlab
 
 # Running Matlab
-matlab Pseudonomas_putidas.mlx
-clear
+sudo matlab
+
+
